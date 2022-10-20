430a84e5ae4a4c1378db069f65e5c329e1ad5a31
Nick <nicolas.cinera@gmail.com>
2022-10-20 14:11:11 -0400

    Cleaning Up Main

--- 4chan/storing_4chan.py
+++ 4chan/storing_4chan.py
@@ -2,39 +2,45 @@ import requests
 import json
 import re, string
 
-r = requests.get('https://a.4cdn.org/g/catalog.json')
-r = r.json()
 
 def clean_text(text):
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
 
-all_posts = dict()
-for page in r:
-    for thread in page['threads']:
-        
-        if 'com' not in thread:
-            continue
 
-        title = thread['com']
-        title = clean_text(title)
-        all_posts[title] = list()
-
-
-        if 'last_replies' not in thread:
-            continue
-
-        for post in thread['last_replies']:
-            if 'com' not in post:
+def main():
+    r = requests.get('https://a.4cdn.org/g/catalog.json')
+    r = r.json()
+    all_posts = dict()
+    for page in r:
+        for thread in page['threads']:
+            
+            if 'com' not in thread:
                 continue
 
-            parsed_post = post['com']
-            parsed_post = clean_text(parsed_post)
-            all_posts[title].append(parsed_post)
-            print(parsed_post)
+            title = thread['com']
+            title = clean_text(title)
+            all_posts[title] = list()
 
 
-print(len(all_posts))
+            if 'last_replies' not in thread:
+                continue
 
-with open('data_chan_games.json', 'w') as f:
-    json.dump(all_posts, f, indent=4)
+            for post in thread['last_replies']:
+                if 'com' not in post:
+                    continue
+
+                parsed_post = post['com']
+                parsed_post = clean_text(parsed_post)
+                all_posts[title].append(parsed_post)
+                print(parsed_post)
+
+
+    print(len(all_posts))
+
+    with open('data_chan_games.json', 'w') as f:
+        json.dump(all_posts, f, indent=4)
+
+
+if __name__ == '__main__':
+    main()
